b5009248a8036415f4599705c002ed808a0d18a3
masal9pse <returnymgstokh@icloud.com>
2025-03-24 21:33:44 +0900

    単一widgetに複数のアニメーションを異なるタイミングで実行する

--- lib/animation/first_animation.dart
+++ lib/animation/first_animation.dart
@@ -24,6 +24,9 @@ class MyWidget extends StatefulWidget {
 class _MyWidgetState extends State<MyWidget>
     with SingleTickerProviderStateMixin {
   late AnimationController controller;
+  // late Tween<Alignment> alignmentTween;
+  late Animatable<Alignment> alignmentTween;
+  late Animatable<double> countTween;
   late Animation<Alignment> animation;
   late Animation<double> countAnimation;
 
@@ -33,27 +36,31 @@ class _MyWidgetState extends State<MyWidget>
     super.initState();
     controller =
         AnimationController(vsync: this, duration: const Duration(seconds: 4));
-    animation = controller.drive(
-      Tween(begin: Alignment.topCenter, end: Alignment.center).chain(
-        CurveTween(
-          curve: Curves.bounceIn,
-        ),
+    alignmentTween =
+        Tween(begin: Alignment.topCenter, end: Alignment.center).chain(
+      CurveTween(
+        curve: Curves.bounceIn,
       ),
     );
-    countAnimation = controller.drive(
-      Tween(begin: 0.0, end: 100.0).chain(
-        CurveTween(
-          curve: Curves.bounceIn,
-        ),
+    countTween = Tween(begin: 0.0, end: 100.0).chain(
+      CurveTween(
+        curve: Curves.bounceIn,
       ),
     );
+    animation = CurvedAnimation(parent: controller, curve: Interval(0, 0.5))
+        .drive(alignmentTween);
+    countAnimation =
+        CurvedAnimation(parent: controller, curve: Interval(0.5, 1.0))
+            .drive(countTween);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: BackButton(onPressed: () => controller.animateBack(0),),
+        leading: BackButton(
+          onPressed: () => controller.animateBack(0),
+        ),
       ),
       body: AnimatedBuilder(
         animation: controller,
